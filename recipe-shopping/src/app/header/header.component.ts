82f1b3178a5ff9b52863097b43738190a8bc7bb8
Eric Zorn <goldencatch93@gmail.com>
2017-11-30 21:07:04 -0500

    Adding Navigation with Event Binding

--- recipe-shopping/src/app/header/header.component.ts
+++ recipe-shopping/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 
   constructor() { }
 
