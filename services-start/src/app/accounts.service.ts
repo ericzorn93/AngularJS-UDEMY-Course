0491ca0f6168779827f1b2eeb962a3d1522d7bd2
Eric Zorn <eric.zorn@quinnipiac.edu>
2017-12-12 00:04:13 -0500

    Injecting a Service into a Service Complete

--- services-start/src/app/accounts.service.ts
+++ services-start/src/app/accounts.service.ts
@@ -23,9 +23,11 @@ export class AccountsService {
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
